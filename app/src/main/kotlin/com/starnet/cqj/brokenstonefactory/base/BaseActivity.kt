c17cb0685ad2f0313a510ce12959afbbd40e89ed
chenqijun <383219087@qq.com>
2017-07-19 15:40:53 +0800

    完善登录界面的图标，交互

--- app/src/main/kotlin/com/starnet/cqj/brokenstonefactory/base/BaseActivity.kt
+++ app/src/main/kotlin/com/starnet/cqj/brokenstonefactory/base/BaseActivity.kt
@@ -3,8 +3,10 @@ package com.starnet.cqj.brokenstonefactory.base
 import android.app.Activity
 import android.os.Bundle
 import android.support.annotation.StringRes
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import org.jetbrains.anko.find
 
 open class BaseActivity : AppCompatActivity() {
 
@@ -12,11 +14,13 @@ open class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
     }
 
-    fun Activity.toast(message:String,length:Int = Toast.LENGTH_SHORT){
-        Toast.makeText(this,message,length).show();
+    fun Activity.toast(message:String,length:Int = Snackbar.LENGTH_SHORT){
+        Snackbar.make(find(android.R.id.content),message,length).show()
+//        Toast.makeText(this,message,length).show();
     }
 
     fun Activity.toast(@StringRes message:Int,length:Int=Toast.LENGTH_SHORT){
-        Toast.makeText(this,message,length).show();
+        Snackbar.make(find(android.R.id.content),message,length).show()
+//        Toast.makeText(this,message,length).show()
     }
 }
